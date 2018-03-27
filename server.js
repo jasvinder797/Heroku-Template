307754b7e3cf172b8ebc888ea9ed1d62b4368909
Jassi797 <jasvinder797@gmail.com>
2018-03-27 09:56:57 +0530

    Second Save

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
-var express = require('express')
-var path = require('path')
- 
-// creating express server
+
+var express = require('express');
 var app = express();
- 
- 
+var path = require("path");
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8000);
+
 app.use(express.static(path.join(__dirname,"static")))
+var users = [];
+io.on('connection', function (socket) {
+
+	console.log("hello : "+socket.id);
+    users.push(socket.id);
+    console.log(users);
+	socket.on('disconnect', function(){
+		console.log("disconnect");
+        for(var i = 0 ; i<users.length; i++){
+            if(users[i]==socket.id);
+            {
+                users.splice(i,1);
+            }
+        }
+        console.log(users);
+	})
+
+	 socket.emit('userList', users);
+	socket.on('hello server', function(data){
+		console.log(data);
+	})
+
+
  
-// setting port number for running server
-var port = process.env.PORT || 8000;
- 
-// starting express server
-app.listen(port, function() {
- console.log("Server is running at : http://localhost:" + port);
 });
