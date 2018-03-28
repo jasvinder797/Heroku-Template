a8480a517a4373e41c1030b216f94c36ff7ed5a7
Jassi797 <jasvinder797@gmail.com>
2018-03-28 16:09:19 +0530

    update2.3

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
+
 var express = require('express');
 var app = express();
 var path = require("path");
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-server.listen(8000);
-
+var port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname,"static")))
-
 io.on('connection', function (socket) {
-socket.on('userName',function(data)
+
+     socket.on('userName',function(userName,color)
      {
        console.log('user'); 
-       socket.emit('userList', data);
-       socket.broadcast.emit('userList', data)
+       socket.emit('userList', userName,color);
+       socket.broadcast.emit('userList', userName,color)
      })
-      socket.on('sendMessage',function(userName,message)
+      socket.on('sendMessage',function(userName,message,color)
      {
        console.log("send"); 
-       socket.emit('displayMsg', userName,message);
-       socket.broadcast.emit('displayMsg', userName,message)
+       socket.emit('displayMsg', userName,message,color);
+       socket.broadcast.emit('displayMsg', userName,message,color)
      })
-	 
+	// socket.on('disconnect', function(){
+	// console.log("disconnect");
+    //        socket.emit('userList', data);
+    //      socket.broadcast.emit('userList', data)
+    //    })	
+});
+
+server.listen(port, function() {
+ console.log("Server is running at : http://localhost:" + port);
 });
